09f3d8ae203a515185936c8791d9a85cbf8c4fdd
root <root@localhost>
2024-10-05 23:42:03 +0530

    auto publish 1728151923059

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    double num1 = 4.9;
+    double num2 = 7.2;
+    double num3 = -3.7;
+
+    std::cout << "Floor of " << num1 << " is: " << floor(num1) << std::endl;
+    std::cout << "Floor of " << num2 << " is: " << floor(num2) << std::endl;
+    std::cout << "Floor of " << num3 << " is: " << floor(num3) << std::endl;
+
+    return 0;
+}
